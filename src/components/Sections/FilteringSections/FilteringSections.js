68bd4e503a6329a061a010fa6dc778d6a37d5515
Cvete Trajkovska <cvetet6@gmail.com>
2019-10-29 15:23:20 +0100

    new files part 2

---
+++ src/components/Sections/FilteringSections/FilteringSections.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+import '../../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+class FilteringSections extends Component {
+    render(){
+        return (
+            <div className={"container-fluid"}>
+               <h1>This is filtering sections</h1>
+               <div className={"row"}>
+               <p>Ovde kategoriite so sliki </p>
+               </div>
+               <div className={"row"}>
+
+               </div>
+             </div>
+    );
+    }
+}
+
+export default FilteringSections;

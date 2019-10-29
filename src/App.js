0f364126734ae9fe5f36efc388a3f38de9fb7664
Cvete Trajkovska <cvetet6@gmail.com>
2019-10-29 15:19:39 +0100

    app file

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import Header from './components/Header';
 import Content from './components/Content'
 import './App.css';
 import Footer from "./components/Footer";
-
+import Menu from "./components/Menu/Menu";
+import VolnterStory from "./components/VolnterStory";
+import Sections from "./components/Sections/Sections";
 class App extends Component {
 
   render(){
     return (
         <div className="App">
-             <Header/>
-             <Content/>
+             <Menu/>
+             <VolnterStory/>
              <Footer/>
-             <div>lemme see</div>
         </div>
     );
   }

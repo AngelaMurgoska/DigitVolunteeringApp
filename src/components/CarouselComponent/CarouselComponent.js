a777257977842f16f301d68dce849676f5ea6aad
Angela Murgoska <angela.murgoska@yahoo.com>
2019-10-29 15:04:26 +0100

    Search, SitePost, Footer

--- src/components/CarouselComponent/CarouselComponent.js
+++ src/components/CarouselComponent/CarouselComponent.js
@@ -1,23 +1,29 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
-import volunteeringImg1 from '../../assets/images/involved_volunteering.jpg'
-import volunteeringImg2 from '../../assets/images/Tips-for-Volunteering-With-Kids.jpg'
-import volunteeringImg3 from '../../assets/images/Volunteering is Good for You and Your Kids.jpg'
+import volunteeringImg1 from '../../assets/images/iStock-1145183123.jpg'
+import volunteeringImg2 from '../../assets/images/microvolunteering-making-a-difference-in-a-matter-of-minutes-588a3926ecd2b.jpeg'
+import volunteeringImg3 from '../../assets/images/1_tKw-v0X5-U9qLtTXqkFbFg.jpeg'
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './CarouselComponent.css'
+import SearchBar from "../SearchBar/SearchBar";
 
 
 export default () => (
-    <div>
+    <div className={"content-position"}>
         <Carousel autoPlay={true} showIndicators={false} showArrows={false} width={"100%"} showThumbs={false}>
             <div>
-                <img src={volunteeringImg2} />
+                <img src={volunteeringImg1} />
             </div>
             <div>
-                <img src={volunteeringImg1}/>
+                <img src={volunteeringImg2}/>
             </div>
             <div>
                 <img src={volunteeringImg3} />
             </div>
         </Carousel>
+        <span className="centered">
+            <SearchBar/>
+        </span>
     </div>
+
 );
